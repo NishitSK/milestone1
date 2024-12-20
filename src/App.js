6add8395abc50b29c067e0ad6130e9b7041f97b1
NishitSK <138647749+NishitSK@users.noreply.github.com>
2024-12-20 21:43:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import Menu from './components/Menu';
-// import Reservations from './components/Reservations';
-// import Contact from './components/Contact';
-// import './App.css';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/menu" element={<Menu />} />
-//           <Route path="/reservations" element={<Reservations />} />
-//           <Route path="/contact" element={<Contact />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Menu from './components/Menu';
+import DishPage from './components/DishPage';
 import Reservations from './components/Reservations';
 import Contact from './components/Contact';
-import AboutUs from './components/About'; // Import the AboutUs component
+import AboutUs from './components/About';
 import './App.css';
 
 const App = () => {
@@ -44,14 +17,17 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
+          <Route path="/dish/:dishId" element={<DishPage />} />
           <Route path="/reservations" element={<Reservations />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<AboutUs />} /> {/* Add About route */}
+          <Route path="/about" element={<AboutUs />} />
         </Routes>
       </div>
     </Router>
   );
 };
+
+export default App;
 
 export default App;
 
